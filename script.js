17ab8aeaed2385df0bbd24b8de33e80ea3f02f84
wintergitweb <pannatron0050@gmail.com>
2024-08-23 22:17:44 +0700

    first commit

--- script.js
+++ script.js
@@ -9,11 +9,15 @@ function toggleHam(x) {
     }
  }
 
- $(window).scroll(function(){
-    if($(window).scrollTop()){
-        $("nav").addClass("black");
+    // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+    window.onscroll = function() {scrollFunction()};
+        
+    function scrollFunction() {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("navbar").style.padding = "4px 10px";
+        document.getElementById("logo").style.fontSize = "25px";
+      } else {
+        document.getElementById("navbar").style.padding = "30px 10px";
+        document.getElementById("logo").style.fontSize = "35px";
+      }
     }
-    else{
-        $("nav").removeClass("black");
-    }
-   })
